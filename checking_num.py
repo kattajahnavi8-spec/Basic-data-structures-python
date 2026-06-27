c533f20918470443db9304392d117b2ce558b771
kattajahnavi8-spec <kattajahnavi8@gmail.com>
2026-06-27 15:09:06 +0530

    Added new program

---
+++ checking_num.py
@@ -0,0 +1,8 @@
+n = input("Enter a number: ")
+n = int(n)
+if n > 0:
+    print("The number is positive.")
+elif n < 0:  
+    print("The number is negative.")
+else:
+    print("The number is zero.")      
